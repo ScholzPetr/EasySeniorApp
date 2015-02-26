11d07026020664753fee39360d021d743c9ec893
ScholzPetr <scholz.pitrs@gmail.com>
2015-02-26 21:09:43 +0100

    getPhone

---
+++ app/src/main/java/com/example/scholzpetr/easyseniorapp/ContactPhone.java
@@ -0,0 +1,14 @@
+package com.example.scholzpetr.easyseniorapp;
+
+/**
+ * Created by ScholzPetr on 22.2.2015.
+ */
+public class ContactPhone {
+    public String number;
+    public String type;
+
+    public ContactPhone(String number, String type) {
+        this.number = number;
+        this.type = type;
+    }
+}
